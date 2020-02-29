9cb6c26e2229d0cb722f89642cc9eae73940b9f7
Denis <Danilkin58@gmail.com>
2020-03-01 01:36:47 +0300

    feat: add functionality for human read number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function toReadable (number) {
-  
+    let dic = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        }
+        
+        if(number in dic)
+            return dic[number];
+        if(number == 0) return 'zero';
+        else 
+        {
+                var arr = Math.abs(number).toString().split('');
+            switch(arr.length){
+                case 2: return dic[parseInt(arr[0] + 0)] + " " + dic[parseInt(arr[1])];
+                break;
+                case 3: if(number%100 == 0) return dic[+arr[0]] + " hundred";
+                        if(parseInt(arr[arr.length - 2] + arr[arr.length-1]) > 20 && !(parseInt(arr[arr.length - 2] + arr[arr.length-1]) in dic))
+                        return dic[parseInt(arr[0])] + " hundred " + dic[parseInt(arr[arr.length-2] + 0)] + " " + dic[parseInt(arr[arr.length-1])];
+                        else return dic[parseInt(arr[0])] + " hundred " + dic[parseInt(arr[arr.length-2] + arr[arr.length-1])];
+                break;
+                case 4: if(number%1000 == 0) return dic[+arr[0]] + " thousand";
+                        if(number%100 == 0) return dic[+arr[0]] + " thousand " + dic[+arr[1]] + " hundred";
+                        if(parseInt(arr[arr.length - 2] + arr[arr.length-1]) > 20 && !(parseInt(arr[arr.length - 2] + arr[arr.length-1]) in dic))
+                        return dic[parseInt(arr[0])] + " thousand " +dic[parseInt(arr[1])] + " hundred " + dic[parseInt(arr[2] + 0)] + " " + dic[parseInt(arr[3])];
+                        else return dic[parseInt(arr[0])] + " thousand " +dic[parseInt(arr[1])] + " hundred " + dic[parseInt(arr[arr.length-2]+arr[arr.length-1])];
+                break;
+            }
+            
+        }           
+
 }
